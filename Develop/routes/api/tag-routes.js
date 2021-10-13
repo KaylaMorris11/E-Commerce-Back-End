d8aee585feb075130a736ef4d52f61798564e2bb
Kayla Morris <moreiskay14@gmail.com>
2021-10-13 00:05:29 -0700

    Fixed an error in delete tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -54,12 +54,18 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one tag by its `id` value
   try {
-    const deleteTag = await Tag.destroy(req.body,{
-      where:{id:req.params.id}
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
     });
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
     res.status(200).json(deleteTag);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
